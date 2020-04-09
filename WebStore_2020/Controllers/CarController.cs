9a56f3f0d3cef98797ddbe7faae62c824fa63cee
Шредер Дмитрий <DimonFX@yandex.ru>
2020-04-09 22:11:34 +0500

    Задание 2. + new Model + Controller with 2 Action + 2 View + communication

---
+++ WebStore_2020/Controllers/CarController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore_2020.Models;
+
+namespace WebStore_2020.Controllers
+{
+    public class CarController : Controller
+    {
+        List<Models.CarModel> _cars;
+
+        public CarController()
+        {
+            _cars = new List<CarModel>
+            {
+                new CarModel
+                {
+                    Id = 1,
+                    BodyType = "Хэтчбек",
+                    Color = "Красный",
+                    Drive= "Передний",
+                    EngineVolume = 1.6,
+                    Mark = "Nissan",
+                    Model = "Almera",
+                    Transmission = "МКПП",
+                    YearRelease = 2018
+                },
+                new CarModel
+                {
+                    Id = 2,
+                    BodyType = "Х",
+                    Color = "Черный",
+                    Drive= "Задний",
+                    EngineVolume = 1.6,
+                    Mark = "ВАЗ 2107",
+                    Model = "LADA",
+                    Transmission = "МКПП",
+                    YearRelease = 2004
+                }
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(_cars);
+        }
+        public IActionResult Details(int Id)
+        {
+            return View(_cars.FirstOrDefault(x=>x.Id==Id));
+        }
+    }
+}
---
+++ WebStore_2020/Models/CarModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore_2020.Models
+{
+    public class CarModel
+    {
+        public int Id { get; set; }
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        /// <summary>
+        /// Тип кузова
+        /// </summary>
+        public string BodyType { get; set; }
+        /// <summary>
+        /// год выпуска
+        /// </summary>
+        public int YearRelease { get; set; }
+        public string Transmission { get; set; }
+        public string Color { get; set; }
+        /// <summary>
+        /// Привод: задний, полный, передний
+        /// </summary>
+        public string Drive { get; set; }
+        /// <summary>
+        /// Объем двигателя
+        /// </summary>
+        public double EngineVolume { get; set; }
+    }
+}
