98eb1f678256ed40edd884477a3a4c87628df5eb
Kiruhin <ikiruhin@bk.ru>
2020-04-12 17:34:34 +0300

    Занятие 2: добавлена мастер-страница

---
+++ WebStore_2020/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebStore_2020.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/blog
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+    }
+}
