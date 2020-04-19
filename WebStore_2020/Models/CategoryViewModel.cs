88c4ca5efe75a7de515bee1ba2e02f566a34389d
Kiruhin <ikiruhin@bk.ru>
2020-04-19 22:47:06 +0300

    Занятие 4: ViewComponent для категорий товаров

---
+++ WebStore_2020/Models/CategoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore_2020.Models
+{
+    public class CategoryViewModel : INamedEntity, IOrderedEntity
+    {
+        public CategoryViewModel()
+        {
+            ChildCategories = new List<CategoryViewModel>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+
+        /// <summary>
+        /// Дочерние секции
+        /// </summary>
+        public List<CategoryViewModel> ChildCategories { get; set; }
+
+        /// <summary>
+        /// Родительская секция
+        /// </summary>
+        public CategoryViewModel ParentCategory { get; set; }
+
+    }
+}
---
+++ WebStore_2020/ViewComponents/CategoriesViewComponent.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore_2020.Infrastructure.Interfaces;
+using WebStore_2020.Models;
+
+namespace WebStore_2020.ViewComponents
+{
+    //[ViewComponent(Name = "Categories")]
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public CategoriesViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var _categories = GetCategories();
+            return View(_categories);
+        }
+
+        private List<CategoryViewModel> GetCategories()
+        {
+            var categories = _productService.GetCategories();
+            // получим и заполним родительские категории
+            var parentSections = categories.Where(p => !p.ParentId.HasValue).ToArray();
+            var parentCategories = new List<CategoryViewModel>();
+            foreach (var parentCategory in parentSections)
+            {
+                parentCategories.Add(new CategoryViewModel()
+                {
+                    Id = parentCategory.Id,
+                    Name = parentCategory.Name,
+                    Order = parentCategory.Order,
+                    ParentCategory = null
+                });
+            }
+
+            // получим и заполним дочерние категории
+            foreach (var CategoryViewModel in parentCategories)
+            {
+                var childCategories = categories.Where(c => c.ParentId == CategoryViewModel.Id);
+                foreach (var childCategory in childCategories)
+                {
+                    CategoryViewModel.ChildCategories.Add(new CategoryViewModel()
+                    {
+                        Id = childCategory.Id,
+                        Name = childCategory.Name,
+                        Order = childCategory.Order,
+                        ParentCategory = CategoryViewModel
+                    });
+                }
+                CategoryViewModel.ChildCategories = CategoryViewModel.ChildCategories.OrderBy(c => c.Order).ToList();
+            }
+            parentCategories = parentCategories.OrderBy(c => c.Order).ToList();
+            return parentCategories;
+        }
+
+    }
+}
