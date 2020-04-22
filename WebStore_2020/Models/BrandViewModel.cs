a1a111af5e00fb0ea2788891f4fc1f2563dc1b04
Kiruhin <ikiruhin@bk.ru>
2020-04-22 21:53:15 +0300

    Занятие 4. ДЗ: ViewComponent для брендов

---
+++ WebStore_2020/Models/BrandViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Models
+{
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Количество товаров бренда
+        /// </summary>
+        public int ProductsCount { get; set; }
+
+        public int Order { get; set; }
+    }
+
+}
---
+++ WebStore_2020/ViewComponents/BrandsViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.ViewComponents
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public BrandsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var brands = GetBrands();
+            return View(brands);
+        }
+
+        private IEnumerable<BrandViewModel> GetBrands()
+        {
+            var dbBrands = _productService.GetBrands();
+            return dbBrands.Select(b => new BrandViewModel
+            {
+                Id = b.Id,
+                Name = b.Name,
+                Order = b.Order,
+                ProductsCount = 0
+            }).OrderBy(b => b.Order).ToList();
+        }
+
+    }
+}
