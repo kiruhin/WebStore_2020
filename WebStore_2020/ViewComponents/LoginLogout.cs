64c2125a16f3fd421a907e63b6d7f382516bc08b
Kiruhin <ikiruhin@bk.ru>
2020-04-26 22:41:10 +0300

    Занятие 6: ViewComponent для логина/регистрации

---
+++ WebStore_2020/ViewComponents/LoginLogout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ViewComponents
+{
+    public class LoginLogout : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
