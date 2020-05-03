ed92ec16c2045e04210816155c080d2f00a635d3
Kiruhin <ikiruhin@bk.ru>
2020-05-03 22:57:54 +0300

    Заняние 8: личный кабинет пользователя с историей заказов

---
+++ WebStore_2020/Controllers/ProfileController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebStore.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IOrdersService _ordersService;
+
+        public ProfileController(IOrdersService ordersService)
+        {
+            _ordersService = ordersService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            var orders = _ordersService.GetUserOrders(User.Identity.Name);
+            var orderModels = new List<UserOrderViewModel>(orders.Count());
+
+            foreach (var order in orders)
+            {
+                orderModels.Add(new UserOrderViewModel()
+                {
+                    Id = order.Id,
+                    Name = order.Name,
+                    Address = order.Address,
+                    Phone = order.Phone,
+                    TotalSum = order.OrderItems.Sum(o => o.Price * o.Quantity)
+                });
+            }
+
+            return View(orderModels);
+        }
+
+    }
+}
---
+++ WebStore_2020/Models/UserOrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class UserOrderViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public decimal TotalSum { get; set; }
+    }
+}
