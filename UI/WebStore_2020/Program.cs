64b84303fdb2a968cd8bd1715255291a3b7bb468
Kiruhin <ikiruhin@bk.ru>
2020-08-10 21:45:43 +0300

    Занятие 4: библиотечка log4net, базовые настройки

--- UI/WebStore_2020/Program.cs
+++ UI/WebStore_2020/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,8 +13,25 @@ namespace WebStore
 {
     public class Program
     {
+        private static readonly log4net.ILog Log =
+            log4net.LogManager.GetLogger(typeof(Program));
+
         public static void Main(string[] args)
         {
+            var log4NetConfig = new XmlDocument();
+ 
+            log4NetConfig.Load(File.OpenRead("log4net.config"));
+ 
+            var repo = log4net.LogManager.CreateRepository(
+                Assembly.GetEntryAssembly(),
+                typeof(log4net.Repository.Hierarchy.Hierarchy));
+ 
+            log4net.Config.XmlConfigurator.Configure(
+                repo,
+                log4NetConfig["log4net"]);
+ 
+            Log.Info("Application - Main is invoked");
+
             CreateHostBuilder(args).Build().Run();
         }
 
