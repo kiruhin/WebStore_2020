a325f9444a75f4c52e4b2c829c6685b6967a317b
Kiruhin <ikiruhin@bk.ru>
2020-08-17 21:16:34 +0300

    Занятие 5: ДЗ. Тесты в CatalogControllerTests

--- Tests/WebStore.XUnitTests/CatalogControllerTests.cs
+++ Tests/WebStore.XUnitTests/CatalogControllerTests.cs
@@ -1,53 +1,119 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebStore.Controllers;
+using WebStore.Domain;
+using WebStore.DomainNew.Dto;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+using Xunit;
 
 namespace WebStore.XUnitTests
 {
     public class CatalogControllerTests
     {
-        // проверим, что метод controller.ProductDetails() возвращает корректную модель
+        [Fact]
         public void ProductDetails_Returns_View_With_Correct_Item()
         {
             // Arrange
-            // заглушка для нужного метода
-            // должна возвращать какой-то список товаров List<ProductDto>
+            var productMock = new Mock<IProductService>();
+            productMock
+                .Setup(p => p.GetProductById(It.IsAny<int>()))
+                .Returns(new ProductDto
+                {
+                    Id = 1,
+                    Name = "Test",
+                    ImageUrl = "TestImage.jpg",
+                    Order = 0,
+                    Price = 10,
+                    Brand = new BrandDto
+                    {
+                        Id = 1,
+                        Name = "TestBrand"
+                    }
+                });
+            var controller = new CatalogController(productMock.Object);
 
             // Act
-            // вызываем тестируемый метод
+            var result = controller.ProductDetails(1);
 
             // Assert
-            // проверим тип пришедших данных
-            // проверим поля модели
+            var viewResult = Xunit.Assert.IsType<ViewResult>(result);
+            var model = Xunit.Assert.IsAssignableFrom<ProductViewModel>(viewResult.ViewData.Model);
+
+            Xunit.Assert.Equal(1, model.Id);
+            Xunit.Assert.Equal("Test", model.Name);
+            Xunit.Assert.Equal(10, model.Price);
+            Xunit.Assert.Equal("TestBrand", model.Brand);
         }
 
-        // метод controller.ProductDetails() должен вернуть 404 NotFound, если не найден товар по id
+        [Fact]
         public void ProductDetails_Returns_NotFound()
         {
             // Arrange
-            // заглушка для метода 
-            // этот метод всегда будет возвращать null
+            var productMock = new Mock<IProductService>();
+            productMock
+                .Setup(p => p.GetProductById(It.IsAny<int>()))
+                .Returns((ProductDto)null);
+            var controller = new CatalogController(productMock.Object);
 
             // Act
+            var result = controller.ProductDetails(1);
 
             // Assert
-            // ответ от контроллера должен быть типа NotFoundResult
+            Xunit.Assert.IsType<NotFoundResult>(result);
         }
 
-        // проверяем корректную работу метода controller.Shop()
+        [Fact]
         public void Shop_Method_Returns_Correct_View()
         {
             // Arrange
-            // заглушка для метода 
-            // должна возвращать какой-то список товаров List<ProductDto>
+            var productMock = new Mock<IProductService>();
+            productMock
+                .Setup(p => p.GetProducts(It.IsAny<ProductFilter>()))
+                .Returns(new List<ProductDto>
+                {
+                    new ProductDto
+                    {
+                        Id = 1,
+                        Name = "Test",
+                        ImageUrl = "TestImage.jpg",
+                        Order = 0,
+                        Price = 10,
+                        Brand = new BrandDto
+                        {
+                            Id = 1,
+                            Name = "TestBrand"
+                        }
+                    },
+                    new ProductDto
+                    {
+                        Id = 2,
+                        Name = "Test2",
+                        ImageUrl = "TestImage2.jpg",
+                        Order = 1,
+                        Price = 22,
+                        Brand = new BrandDto
+                        {
+                            Id = 1,
+                            Name = "TestBrand"
+                        }
+                    }
+                });
+            var controller = new CatalogController(productMock.Object);
 
             // Act
-            // вызываем тестируемый метод
+            var result = controller.Shop(1, 5);
 
             // Assert
-            // проверяем тип данных
-            // проверяем поля модели
-        }
+            var viewResult = Xunit.Assert.IsType<ViewResult>(result);
+            var model = Xunit.Assert.IsAssignableFrom<CatalogViewModel>(viewResult.ViewData.Model);
 
+            Xunit.Assert.Equal(2, model.Products.Count());
+            Xunit.Assert.Equal(5, model.BrandId);
+            Xunit.Assert.Equal(1, model.CategoryId);
+            Xunit.Assert.Equal("TestImage2.jpg", model.Products.ToList()[1].ImageUrl);
+        }
     }
 }
