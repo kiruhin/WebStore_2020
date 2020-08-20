11fb6f17d48ca70a0ab3fa272ac7dfdd43cbd7b5
Kiruhin <ikiruhin@bk.ru>
2020-08-20 21:36:29 +0300

    Занятие 7: добавлен компонент «CartSummary»

---
+++ UI/WebStore_2020/ViewComponents/CartSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.ViewComponents
+{
+    public class CartSummary : ViewComponent
+    {
+        private readonly ICartService _cartService;
+
+        public CartSummary(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IViewComponentResult Invoke() =>
+            View(_cartService.TransformCart());
+    }
+}
